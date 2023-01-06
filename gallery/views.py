b58d4415955d242a8737d841b515bda26c54f311
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-06 09:40:03 -0300

    render template from view.

--- gallery/views.py
+++ gallery/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Hello world. Fedora is nice</p>')
+    return render(request, 'index.html')
