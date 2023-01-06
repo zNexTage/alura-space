5b4b849b00bae37375f1e223d86f99fc0a5fde8c
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-06 09:33:39 -0300

    isolating gallery urls.

--- setup/urls.py
+++ setup/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from gallery.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('gallery/', include('gallery.urls')), # include gallery urls
 ]
