e77959d1041cc5788a18a6951c22d37c76961508
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-13 09:13:59 -0300

    list all photos from database.

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from gallery.models import Photography
+
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'gallery/index.html')
+    photos = Photography.objects.all()
+
+    context = {
+        'photos': photos
+    }
+
+    return render(request, 'gallery/index.html', context)
 
 def image(request):
     return render(request, 'gallery/image.html')
