f6cf88926aeab1144d48ae6b32e98b6d65eeff4d
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-13 08:48:02 -0300

    created Photography model.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Photography(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    name.verbose_name = 'Nome'
+
+    legend = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    legend.verbose_name = 'Legenda'
+
+    description = models.TextField(
+        null=False,
+        blank=False
+    )
+    description.verbose_name = 'Descrição'
+
+    photo = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    photo.verbose_name = 'Foto'
+
+    def __str__(self) -> str:
+        return f'Fotografia - @{self.name}'
+
+
+
