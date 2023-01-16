4cd795bc3e08d58e7d4c739070335744b471e02c
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-16 11:08:10 -0300

    added category field.

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,19 @@ class Photography(models.Model):
     class Meta:
         verbose_name = 'Fotografia'
         verbose_name_plural = 'Fotografias'
+    
+
+    NEBULA = 1
+    STAR = 2
+    GALAXY = 3
+    PLANET = 4
+
+    CATEGORY_CHOICES = (
+        (NEBULA, 'Nebulosa'),
+        (STAR, 'Estrela'),
+        (GALAXY, 'Galáxia'),
+        (PLANET, 'Planeta'),
+    )
 
     name = models.CharField(
         max_length=100,
@@ -32,6 +45,13 @@ class Photography(models.Model):
         blank=False
     )
     photo.verbose_name = 'Foto'
+
+    category = models.CharField(
+        max_length=100,
+        choices=CATEGORY_CHOICES,
+        default=''
+    )
+    category.verbose_name = 'Categoria'
 
     def __str__(self) -> str:
         return f'Fotografia - @{self.name}'
