dc112cc3a4c56837b5c502281f05848155fdf02a
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-19 21:55:57 -0300

    added is_published in admin;
    added in list_display and list_editable.

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from gallery.models import Photography
 
 class PhotographyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'legend')
+    list_display = ('name', 'legend', 'is_published')
     #list_display_links = '' clicable itens;
     search_fields = ('name',)
     list_filter = ('category',)
     list_per_page = 10 # ten itens per page
+    list_editable = ('is_published',) # editable fields in table
 
 # Register your models here.
 admin.site.register(Photography, PhotographyAdmin)
