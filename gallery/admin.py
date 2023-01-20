256b3edbc930d549fb2ceeceb770f6bfe05198e7
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-19 21:50:19 -0300

    added list_filter and list_per_page in admin.

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,6 +6,8 @@ class PhotographyAdmin(admin.ModelAdmin):
     list_display = ('name', 'legend')
     #list_display_links = '' clicable itens;
     search_fields = ('name',)
+    list_filter = ('category',)
+    list_per_page = 10 # ten itens per page
 
 # Register your models here.
 admin.site.register(Photography, PhotographyAdmin)
