c4d08109d6f7c9d6d5cd3a892e3a1631d51d8a76
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-25 08:19:18 -0300

    config media path

--- setup/settings.py
+++ setup/settings.py
@@ -137,6 +137,11 @@ STATICFILES_DIRS = [
 # STATIC_ROOT -> absolute path for python to collect the static files.
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
