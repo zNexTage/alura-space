25c22f2bc92e266fe80e13fc701adb087e2247f0
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-02-10 09:27:35 -0300

    implement a register form.

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 from users.forms import LoginForm, RegisterForm
 
 # Create your views here.
@@ -18,8 +21,40 @@ def login(request):
     )
 
 
+
 def add(request):
-    form = RegisterForm()
+    ''' Accept GET and POST methods
+        POST -> Register an user;
+        GET -> Return the register form.
+    '''
+    
+    data = request.POST or None
+
+    form = RegisterForm(data)
+
+    if request.method == 'POST':
+
+        if form.is_valid():
+
+            if form['password_1'].value() != form['password_2'].value():
+                return redirect(reverse('users:add'))
+
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password_1')
+
+            if User.objects.filter(username=name).exists():
+                return redirect(reverse('users:add'))
+
+            user = User.objects.create_user(
+                username=name,
+                email=email,
+                password=password
+            )
+
+            user.save()
+
+            return redirect(reverse('users:login'))
 
     context = {
         'form': form
