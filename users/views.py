0bd84ee491017aba35ca3717be19787529413539
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-02-13 20:37:41 -0300

    implemented login view.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 from django.urls import reverse
 
 from users.forms import LoginForm, RegisterForm
@@ -8,7 +9,26 @@ from users.forms import LoginForm, RegisterForm
 
 
 def login(request):
-    form = LoginForm()
+    data = request.POST or None
+    form = LoginForm(data)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(
+                request,
+                username=name,
+                password=password
+            )
+
+            if user is not None:
+                auth.login(request, user)
+
+                return redirect(reverse('gallery:index'))
+            else:
+                return redirect(reverse('users:login'))
 
     context = {
         'form': form
